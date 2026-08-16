d3ff7865a3bfa7cbf80ef9d822615a2335c59cfe
dharmi04 <dharmip060@gmail.com>
2026-08-16 23:07:53 +0530

    Use Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
-import AnalyticsTracker from "./components/AnalyticsTracker.jsx";
 import Home from "./pages/Home.jsx";
 import Inquiry from "./pages/Inquiry.jsx";
 import Gallery from "./pages/Gallery.jsx";
@@ -11,7 +11,6 @@ import SignIn from "./pages/SignIn.jsx";
 export default function App() {
   return (
     <div className="flex min-h-screen flex-col bg-[#f7f9f4]">
-      <AnalyticsTracker />
       <Navbar />
       <main className="flex-1">
         <Routes>
@@ -23,6 +22,7 @@ export default function App() {
         </Routes>
       </main>
       <Footer />
+      <Analytics />
     </div>
   );
 }
